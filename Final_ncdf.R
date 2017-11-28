cf1e8578551c249a179aba411faf5a2d1f554749
Behringerdp <Behringerdp@gmail.com>
2017-11-28 16:13:54 -0600

    completed script for netCDF file

--- Final_ncdf.R
+++ Final_ncdf.R
@@ -1,1 +1,23 @@
-#test
+#use data from netCDF for bathymetry of Barataria Bay. 
+
+install.packages("chron")
+install.packages("RcolorBrewer")
+install.packages("lattice")
+install.packages("ncdf")
+install.packages("installr")
+install.packages("ncdf4")
+
+library(ncdf4)
+
+f <- list.files(pattern=".nc",full.names=F)
+
+
+ncname <- "etopo1"
+ncfname <- paste(ncname, ".nc", sep = "")
+dname <- "CHL"  # note: tmp means temperature (not temporary)
+
+
+
+# open a NetCDF file
+ncin <- nc_open(ncfname)
+print(ncin)
