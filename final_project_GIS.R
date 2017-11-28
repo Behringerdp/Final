2dd107d38c37c2d8071c3eae7964af7ab3b10870
Behringerdp <Behringerdp@gmail.com>
2017-11-28 16:17:43 -0600

    subsetted NOAA data to BA bay and merged files so that data had station lat and long

--- final_project_GIS.R
+++ final_project_GIS.R
@@ -1,1 +1,11 @@
 #script for GIS_project
+#NOAA station and shrimp data
+BA_stations=subset(Louisiana_Stations, Louisiana_Stations$Station_Code >= 278 & Station_Code <=322)
+BA_samples=subset(Louisiana_Samples, Louisiana_Samples$Station_Code >=278 & Station_Code <=322)
+BA_samples_2007=subset(BA_samples, BA_samples$YYYY >=2007)
+
+BA_ss = merge(BA_stations, BA_samples, 'Station_Code')
+BA_sal_hydro = merge(BA_ss, Louisiana_Hydrological, 'Sample_Code')
+
+BA_station_sal=subset(BA_sal_hydro, BA_sal_hydro$Station_Code >= 278 & Station_Code <=322)
+BA_AUG_sal=subset(BA_sal_hydro, BA_sal_hydro$MM==8)
